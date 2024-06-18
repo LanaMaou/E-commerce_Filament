5c47b2ceb18962d7a95d6bac9cc1b8be6990913a
LanaMaou <maulanarasyit777@gmail.com>
2024-06-18 12:45:02 +0800

    Make a dynamic product detail

--- app/Livewire/ProductsPage.php
+++ app/Livewire/ProductsPage.php
@@ -2,12 +2,24 @@
 
 namespace App\Livewire;
 
+use App\Models\Brand;
+use App\Models\Category;
+use App\Models\Product;
+use Livewire\Attributes\Title;
 use Livewire\Component;
+use Livewire\WithPagination;
 
+#[Title('Products - Lana Ecommerce')]
 class ProductsPage extends Component
 {
+    use WithPagination;
     public function render()
     {
-        return view('livewire.products-page');
+        $productQuery = Product::query()->where('is_active', 1);
+        return view('livewire.products-page', [
+            'products' => $productQuery->paginate(6),
+            'brands' => Brand::where('is_active', 1)->get(['id', 'name', 'slug']),
+            'categories' => Category::where('is_active', 1)->get(['id', 'name', 'slug']),
+        ]);
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -20,7 +20,7 @@ Route::get('/', HomePage::class);
 Route::get('/categories', CategoriesPage::class);
 Route::get('/products', ProductsPage::class);
 Route::get('/cart', CartPage::class);
-Route::get('/products/{product}', ProductDetailPage::class);
+Route::get('/products/{slug}', ProductDetailPage::class);
 
 Route::get('/checkout', CheckoutPage::class);
 Route::get('/my-orders', MyOrdersPage::class);
