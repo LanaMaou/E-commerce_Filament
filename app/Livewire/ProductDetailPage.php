d95f81be814d1ca4ba5aae6f48d47ce055a6352f
LanaMaou <maulanarasyit777@gmail.com>
2024-06-22 17:45:11 +0800

    Completed function addCart in product detail

--- app/Livewire/ProductDetailPage.php
+++ app/Livewire/ProductDetailPage.php
@@ -2,19 +2,51 @@
 
 namespace App\Livewire;
 
+use App\Helpers\CartManagement;
+use App\Livewire\Partials\Navbar;
 use App\Models\Product;
+use Jantinnerezo\LivewireAlert\LivewireAlert;
 use Livewire\Attributes\Title;
 use Livewire\Component;
 
 #[Title('Product Detail - Lana Ecommerce')]
 class ProductDetailPage extends Component
 {
+    use LivewireAlert;
+
     public $slug;
+    public $quantity = 1;
 
     public function mount($slug)
     {
         $this->slug = $slug;
     }
+
+    public function incrementQuantity()
+    {
+        $this->quantity++;
+    }
+
+    public function decrementQuantity()
+    {
+        if ($this->quantity > 1) {
+            $this->quantity--;
+        }
+    }
+
+    public function addToCart($productId)
+    {
+        $total_count = CartManagement::addItemToCart($productId);
+
+        $this->dispatch('update-cart-count', total_count: $total_count)->to(Navbar::class);
+
+        $this->alert('success', 'Product added to the cart successfully!', [
+            'position' => 'bottom-end',
+            'timer' => 3000,
+            'toast' => true,
+        ]);
+    }
+
     public function render()
     {
         return view('livewire.product-detail-page', [
