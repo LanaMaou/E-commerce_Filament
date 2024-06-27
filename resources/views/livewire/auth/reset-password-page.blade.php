0e85309d3effa405ad899a0640487968be40a319
LanaMaou <maulanarasyit777@gmail.com>
2024-06-27 21:02:17 +0800

    Completed reset password feature

--- resources/views/livewire/auth/reset-password-page.blade.php
+++ resources/views/livewire/auth/reset-password-page.blade.php
@@ -9,25 +9,37 @@
 
                     <div class="mt-5">
                         <!-- Form -->
-                        <form>
+                        <form wire:submit.prevent='save'>
+
+                            @if (session('error'))
+                                <div class="mt-2 bg-red-500 text-sm text-white rounded-lg p-4 mb-4" role="alert">
+                                    {{ session('error') }}
+                                </div>
+                            @endif
+
                             <div class="grid gap-y-4">
                                 <!-- Form Group -->
                                 <div>
                                     <label for="password" class="block mb-2 text-sm dark:text-white">Password</label>
                                     <div class="relative">
-                                        <input type="password" id="password" name="password"
+                                        <input type="password" id="password" wire:model="password"
                                             class="block w-full px-4 py-3 text-sm border border-gray-200 rounded-lg focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-slate-900 dark:border-gray-700 dark:text-gray-400 dark:focus:ring-gray-600"
-                                            required aria-describedby="email-error">
-                                        <div
-                                            class="absolute inset-y-0 flex items-center hidden pointer-events-none end-0 pe-3">
-                                            <svg class="w-5 h-5 text-red-500" width="16" height="16"
-                                                fill="currentColor" viewBox="0 0 16 16" aria-hidden="true">
-                                                <path
-                                                    d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM8 4a.905.905 0 0 0-.9.995l.35 3.507a.552.552 0 0 0 1.1 0l.35-3.507A.905.905 0 0 0 8 4zm.002 6a1 1 0 1 0 0 2 1 1 0 0 0 0-2z" />
-                                            </svg>
-                                        </div>
+                                            aria-describedby="email-error">
+                                        @error('password')
+                                            <div
+                                                class="absolute inset-y-0 flex items-center pointer-events-none end-0 pe-3">
+                                                <svg class="w-5 h-5 text-red-500" width="16" height="16"
+                                                    fill="currentColor" viewBox="0 0 16 16" aria-hidden="true">
+                                                    <path
+                                                        d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM8 4a.905.905 0 0 0-.9.995l.35 3.507a.552.552 0 0 0 1.1 0l.35-3.507A.905.905 0 0 0 8 4zm.002 6a1 1 0 1 0 0 2 1 1 0 0 0 0-2z" />
+                                                </svg>
+                                            </div>
+                                        @enderror
+
                                     </div>
-                                    <p class="hidden mt-2 text-red-600" id="password-error">Password error message</p>
+                                    @error('password')
+                                        <p class="mt-2 text-red-600" id="password-error">{{ $message }}</p>
+                                    @enderror
                                 </div>
                                 <!-- End Form Group -->
 
@@ -35,21 +47,26 @@
                                     <label for="password_confirmation"
                                         class="block mb-2 text-sm dark:text-white">Confirm Password</label>
                                     <div class="relative">
-                                        <input type="password" id="password_confirmation" name="password_confirmation"
+                                        <input type="password" id="password_confirmation"
+                                            wire:model="password_confirmation"
                                             class="block w-full px-4 py-3 text-sm border border-gray-200 rounded-lg focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-slate-900 dark:border-gray-700 dark:text-gray-400 dark:focus:ring-gray-600"
-                                            required aria-describedby="email-error">
+                                            aria-describedby="email-error">
 
-                                        <div class="inset-y-0 flex items-center hidden pointer-events-none end-0 pe-3">
-                                            <svg class="w-5 h-5 text-red-500" width="16" height="16"
-                                                fill="currentColor" viewBox="0 0 16 16" aria-hidden="true">
-                                                <path
-                                                    d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM8 4a.905.905 0 0 0-.9.995l.35 3.507a.552.552 0 0 0 1.1 0l.35-3.507A.905.905 0 0 0 8 4zm.002 6a1 1 0 1 0 0 2 1 1 0 0 0 0-2z" />
-                                            </svg>
-                                        </div>
+                                        @error('password_confirmation')
+                                            <div class="inset-y-0 flex items-center pointer-events-none end-0 pe-3">
+                                                <svg class="w-5 h-5 text-red-500" width="16" height="16"
+                                                    fill="currentColor" viewBox="0 0 16 16" aria-hidden="true">
+                                                    <path
+                                                        d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM8 4a.905.905 0 0 0-.9.995l.35 3.507a.552.552 0 0 0 1.1 0l.35-3.507A.905.905 0 0 0 8 4zm.002 6a1 1 0 1 0 0 2 1 1 0 0 0 0-2z" />
+                                                </svg>
+                                            </div>
+                                        @enderror
 
                                     </div>
-                                    <p class="mt-2 text-xs text-red-600" id="password_confirmation-error">Confirm
-                                        Password Error</p>
+                                    @error('password_confirmation')
+                                        <p class="mt-2 text-xs text-red-600" id="password_confirmation-error">
+                                            {{ $message }}</p>
+                                    @enderror
                                 </div>
 
                                 <button type="submit"
